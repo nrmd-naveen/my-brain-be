e3646c3f5854f95900c8900b1f58b509c1da0cbb
nrmd-naveen <nrmdnaveen@gmail.com>
2025-02-17 23:10:29 +0530

    cros origin set to be all

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import ContentRouter from "./routes/contentRoutes";
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["*"],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }))
